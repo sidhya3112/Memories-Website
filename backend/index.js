d5e696afc5acefddf9f3cacda83521d58d26c559
Sidhya Jain <sidhyajain@gmail.com>
2022-06-16 23:53:18 +0530

    Setting up Redux

--- backend/index.js
+++ backend/index.js
@@ -6,11 +6,13 @@ import postRoutes from './routes/posts.js';
 
 const app = express();
 
-app.use('/posts', postRoutes);
+
 
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
+
+app.use('/posts', postRoutes);
 
 const CONNECTION_URL = 'mongodb+srv://admin:adminpassword@cluster0.gtoaz.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
 import memories from './images/memories.png';
 import Posts from "./components/Posts/Posts";
 import Form from "./components/Form/Form";
 import useStyles from './styles';
+import { useDispatch } from "react-redux";
+import { getPosts } from './actions/posts';
 
 const App = () => {
     const classes = useStyles();
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getPosts());
+    }, [dispatch]);
+
     return(
         <Container maxwidth="lg">
             <AppBar className={classes.appBar} position="static" color="inherit" >
