c082acf9df68c05635416570c034e0ba2977d1f8
Sidhya Jain <sidhyajain@gmail.com>
2022-06-17 03:35:54 +0530

    Displaying Posts

--- frontend/src/components/Posts/Posts.js
+++ frontend/src/components/Posts/Posts.js
@@ -1,21 +1,25 @@
-import React from "react";
-import Post from "./Post/Post";
+import React from 'react';
+import { Grid, CircularProgress } from '@material-ui/core';
+import { useSelector } from 'react-redux';
+
+import Post from './Post/Post';
 import useStyles from './styles';
-import { useSelector } from "react-redux";
 
 const Posts = () => {
-    const posts = useSelector((state) => state.posts);
-    const classes = useStyles();
-    console.log(posts);
-    return(
-        <>
-            <h1>POSTS</h1>
-            <Post/>
-            <Post/>
+  const posts = useSelector((state) => state.posts);
+  const classes = useStyles();
 
-        </>
-        
-    );
-}
+  return (
+    !posts.length ? <CircularProgress /> : (
+      <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+        {posts.map((post) => (
+          <Grid key={post._id} item xs={12} sm={6} md={6}>
+            <Post post={post} />
+          </Grid>
+        ))}
+      </Grid>
+    )
+  );
+};
 
 export default Posts;
