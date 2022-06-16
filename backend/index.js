f612581aefb9da9fba2dba156366e244387f04cc
Sidhya Jain <sidhyajain@gmail.com>
2022-06-16 16:30:51 +0530

    Initial commit

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import postRoutes from './routes/posts.js';
+
+const app = express();
+
+app.use('/posts', postRoutes);
+
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://admin:adminpassword@cluster0.gtoaz.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => app.listen(PORT, () => console.log(`Server is running on port ${PORT}`)))
+.catch((error) => console.log(error.message));
+
